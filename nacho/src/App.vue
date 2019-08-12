77252508edf203be5041eceeded04b80c94545b4
Mauro Sarabia <maurosarabia@Jennys-MacBook-Pro.local>
2019-08-12 16:29:46 -0500

    started building app

---
+++ nacho/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Nacho</h1>
+
+    <nacho-table />
+  </div>
+</template>
+
+<script>
+import NachoTable from './components/NachoTable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NachoTable,
+  }
+}
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px
+}
+</style>
---
+++ nacho/src/components/NachoTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="nacho-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Ignacio Sarabia</td>
+                    <td>ignacio.sarabia@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>Angélica García</td>
+                    <td>angelica.garcia@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>Jacobo Wick</td>
+                    <td>jacobo.wick@gmail.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'nacho-table'
+}
+</script>
+
+<style scoped></style>
